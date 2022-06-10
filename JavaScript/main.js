2ab87c2b8ce93ed1eb818fb5a69ea5cf64f155a8
rodrialeh01 <2823897250101@ingenieria.usac.edu.gt>
2022-06-10 15:02:52 -0600

    Creacion de la matriz dispersa

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,5 +1,5 @@
 //=======================================================USUARIOS===================================================
-//LISTA CIRCULAR Y LISTA DE LISTAS
+//------------LISTA CIRCULAR Y LISTA DE LISTAS------------
 class NodoCU{
     constructor(_usuario){
         this.usuario = _usuario;
@@ -82,7 +82,7 @@ class ListaUsuarios{
     }
 }
 
-//CLASE DE TIPO USUARIO
+//---------CLASE DE TIPO USUARIO-------------
 class Usuario{
     constructor(_dpi,_nombre,_username,_correo,_rol,_contrasenia,_telefono){
         this.dpi = _dpi;
@@ -94,6 +94,7 @@ class Usuario{
         this.telefono = _telefono
     }
 }
+//-------------------LOGICA DE USUARIOS--------------------
 //IMPLEMENTACION DE LA LISTA DE USUARIOS
 var users = new ListaUsuarios()
 localStorage.setItem("lista-usuarios",users)
@@ -131,6 +132,158 @@ function CargaMasivaUsuarios(){
 }
 
 //================================================LIBROS===================================
+
+//-----------MATRIZ DISPERSA------------
+class NodoCabeceraMD{
+    constructor(id){
+        this.id = id
+        this.siguiente = null
+        this.anterior = null
+        this.acceso = null
+    }
+}
+class ListaCabeceraMD{
+    constructor(tipo){
+        this.tipo = tipo
+        this.primero = null
+        this.ultimo = null
+        this.tamanio = 0
+    }
+
+    insertar_nodoCabecera(nuevo){
+        if (this.primero == null){
+            this.primero = nuevo
+            this.ultimo = nuevo
+        }else{
+            if (nuevo.id < this.primero.id){
+                nuevo.siguiente = this.primero
+                this.primero.anterior = nuevo
+                this.primero = nuevo
+            }else if(nuevo.id > this.ultimo.id){
+                this.ultimo.siguiente = nuevo
+                nuevo.anterior = this.ultimo
+                this.ultimo = nuevo
+            }else{
+                let temporal = this.primero
+                while (temporal != null){
+                    if(nuevo.id < temporal.id){
+                        nuevo.siguiente = temporal
+                        nuevo.anterior = temporal.anterior
+                        temporal.anterior.siguiente = nuevo
+                        temporal.anterior = nuevo
+                        break
+                    }else if(nuevo.id>temporal.id){
+                        temporal = temporal.siguiente
+                    }else{
+                        break
+                    }
+                }
+            }
+        }
+    }
+    obtenercabecera(id){
+        let temporal = this.primero
+        while(temporal != null){
+            if (id == temporal.id){
+                return temporal
+            }
+            temporal = temporal.siguiente
+        }
+        return null
+    }
+}
+class NodoCelda{
+    constructor(x,y,libro){
+        this.x = x
+        this.y = y
+        this.libro = libro
+        this.arriba = null
+        this.abajo = null
+        this.izquierda = null
+        this.derecha = null
+    }
+}
+class MatrizDispersa{
+    constructor(){
+        this.filas = new ListaCabeceraMD('fila')
+        this.columnas = ListaCabeceraMD('columna')
+    }
+    insertar(coor_x,coor_y,libro){
+        let nuevo = new NodoCelda(coor_x,coor_y, libro)
+        let celda_x = this.filas.obtenercabecera(coor_x)
+        let celda_y = this.columnas.obtenercabecera(coor_y)
+
+        if(celda_x==null){
+            celda_x = NodoCabeceraMD(coor_x)
+            this.filas.insertar_nodoCabecera(celda_x)
+        }
+        if(celda_y == null){
+            celda_y = NodoCabeceraMD(coor_y)
+            this.columnas.insertar_nodoCabecera(celda_y)
+        }
+
+        if(celda_x.acceso == null){
+            celda_x.acceso = nuevo
+        }else{
+            if(nuevo.y < celda_x.acceso.y){
+                nuevo.derecha = celda_x.acceso
+                celda_x.acceso.izquierda = nuevo
+                celda_x.acceso = nuevo
+            }else{
+                let temporal = celda_x.acceso
+                while(temporal != null){
+                    if(nuevo.y < temporal.y){
+                        nuevo.derecha = temporal
+                        nuevo.izquierda = temporal.izquierda
+                        temporal.izquierda.derecha = nuevo
+                        temporal.izquierda = nuevo
+                        break
+                    }else if(nuevo.x == temporal.x && nuevo.y == temporal.y){
+                        break
+                    }else{
+                        if(temporal.derecha == null){
+                            temporal.derecha = nuevo
+                            nuevo.izquierda = temporal
+                            break
+                        }else{
+                            temporal = temporal.derecha
+                        }
+                    }
+                }
+            }
+        }
+
+        if (celda_y.acceso == null){
+            celda_y.acceso = nuevo
+        }else{
+            if(nuevo.x < celda_y.acceso.x){
+                nuevo.abajo = celda_y.acceso
+                celda_y.acceso.arriba = nuevo
+                celda_y.acceso = nuevo
+            }else{
+                let tempo = celda_y.acceso
+                while(tempo!= null){
+                    if(nuevo.x<tempo.x){
+                        nuevo.abajo = tempo
+                        nuevo.arriba = tempo.arriba
+                        tempo.arriba.abajo = nuevo
+                        tempo.arriba = nuevo
+                    }else if(nuevo.x == tempo.x && nuevo.y == tempo.y){
+                        break
+                    }else{
+                        if(tempo.abajo == null){
+                            tempo.abajo = nuevo
+                            nuevo.arriba = tempo
+                            break
+                        }else{
+                            tempo = tempo.abajo
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
 //CARGA MASIVA DE LIBROS
 function CargaLibros(){
